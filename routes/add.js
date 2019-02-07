cbf516e48988bc70ed50f55313a0cecf38dd66c3
AllysonLlacuna <allacuna@ucsd.edu>
2019-02-07 13:22:54 -0800

    HCI Friends

--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,9 @@ var data = require("../data.json");
 
 exports.addFriend = function(request, response) {    
 	// Your code goes here
+	console.log(data);
+	response.render('index', data);
+
+	data.friends.push(newFriend);
+
  }
